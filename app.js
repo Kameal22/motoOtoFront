7c8598492ba17aacd680b8ceafbb558d4aa29342
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-08-23 14:48:16 +0200

    Added logout button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const loginSection = document.querySelector('#logIn');
 const userNameSection = document.querySelector('#userNameSection');
 const userNameText = document.querySelector('#nickName');
+const logoutBtn = document.querySelector('#logoutBtn');
 
 if(localStorage.length === 1){
     const usersNickname = getParsedJwt(rawToken);
@@ -12,3 +13,8 @@ if(localStorage.length === 1){
     loginSection.style.display = "flex";
     userNameSection.style.display = "none";
 }
+
+logoutBtn.addEventListener('click', () =>{
+    window.localStorage.removeItem('userToken');
+    window.location.reload();
+})
