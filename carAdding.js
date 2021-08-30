1a3d9056379e413580a368ee8480c0be679f9185
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-08-30 13:18:16 +0200

    car adding progress

--- carAdding.js
+++ carAdding.js
@@ -6,16 +6,23 @@ const modelAddingSelect = document.querySelector('#modelAdding');
 const generationAddingSelect = document.querySelector('#generationAdding');
 const fuelAddingSelect = document.querySelector('#fuelAdding');
 const capacityAddingSelect = document.querySelector('#capacityAdding');
-const conditionAddingSelect = document.querySelector('#isDamagedAdd');
-const originAddingSelect = document.querySelector('#originAdding');
+const mileageInput = document.querySelector('#mileageInput');
+const priceInput = document.querySelector('#priceInput');
 
 const sendAddFormBtn = document.querySelector('#sendAddForm');
 
-brandAddingSelect.addEventListener('change', (event) => {
-    // console.log(event.target.value);
-});
+addingForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
 
-// Getting car brand and model from API below
+const actualYear = new Date().getFullYear();
+let options = "";
+
+for (let y = actualYear; y >= 1980; y--) {
+    options += "<option>" + y + "</option>"
+}
+
+productionYearSelect.innerHTML = options
 
 axios.get('http://localhost:8080/api/cars/brands', )
     .then(function(response) {
@@ -32,32 +39,78 @@ axios.get('http://localhost:8080/api/cars/brands', )
             brandAddingSelect.appendChild(newBrandOption);
         });
 
-        axios.get('http://localhost:8080/api/cars/' + audiBrand + '/models', )
-            .then(function(response) {
-                const audiModels = response.data
-                for (let i = 0; i < audiModels.length; i++) {
-                    const model = audiModels[i]
-                }
-            })
-
-        .catch(function(error) {
-            console.log(error);
-
-        });
-
-        axios.get('http://localhost:8080/api/cars/' + bmwBrand + '/models', )
-            .then(function(response) {
-                const bmwModels = response.data
-                    // console.log(bmwModels);
-            })
-
-        .catch(function(error) {
-            console.log(error);
-
-        });
     })
 
 .catch(function(error) {
     console.log(error);
 
 });
+
+
+
+brandAddingSelect.addEventListener('change', (event) => {
+    const chosenBrand = event.target.value
+
+    carAddedOptions["brand"] = chosenBrand;
+
+    axios.get('http://localhost:8080/api/cars/' + chosenBrand + '/models', )
+        .then(function(response) {
+
+            modelAddingSelect.innerHTML = "";
+
+            const chosenModels = response.data
+
+            chosenModels.forEach((model) => {
+                const newModelOption = document.createElement('option');
+                const modelOptionText = document.createTextNode(model);
+
+                newModelOption.appendChild(modelOptionText);
+
+                newModelOption.setAttribute('value', model);
+
+                modelAddingSelect.appendChild(newModelOption);
+            })
+
+            modelAddingSelect.addEventListener('change', (event) => {
+                const selectedModel = event.target.value
+                carAddedOptions["model"] = selectedModel
+            })
+
+        })
+
+    .catch(function(error) {
+        console.log(error);
+
+    });
+});
+
+productionYearSelect.addEventListener('change', (event) => {
+    const selectedYear = event.target.value;
+    carAddedOptions["productionYear"] = selectedYear;
+})
+
+generationAddingSelect.addEventListener('change', (event) => {
+    const selectedGeneration = event.target.value;
+    carAddedOptions["generation"] = selectedGeneration;
+})
+
+fuelAddingSelect.addEventListener('change', (event) => {
+    const selectedFuel = event.target.value;
+    carAddedOptions["fuelType"] = selectedFuel;
+})
+
+mileageInput.addEventListener('input', (event) => {
+    const carMileage = parseInt(event.target.value);
+    carAddedOptions["carMileage"] = carMileage;
+})
+
+priceInput.addEventListener('input', (event) => {
+    const carPrice = parseInt(event.target.value);
+    carAddedOptions["price"] = `${carPrice} $`;
+})
+
+// Chosen car adding options :
+
+carAddedOptions = {
+
+}
