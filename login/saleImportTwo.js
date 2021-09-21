bb5574065ff83abad3c32c1a7335da6f12ee4e88
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-09-21 16:00:15 +0200

    landing page progress

--- login/saleImportTwo.js
+++ login/saleImportTwo.js
@@ -6,7 +6,9 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=bmw`)
     .then(function(response) {
         const importedBmwCars = response.data
 
-        importedBmwCars.forEach((bmwCar) => {
+        console.log(importedBmwCars);
+
+        importedBmwCars.slice(-2).forEach((bmwCar) => {
 
             //Creates two bmw cars div - main div
             const bmwAnnouncementsDiv = document.createElement('div');
@@ -23,25 +25,17 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=bmw`)
             const bmwDescriptions = document.createElement('div');
 
             //Two description smaller divs
-            const divBMV = document.createElement('div');
-            const divYMF = document.createElement('div');
+            const divBMYF = document.createElement('div');
 
             //Smaller description divs children
             const firstB = document.createElement('h2');
             const firstM = document.createElement('h2');
-            const firstV = document.createElement('h2');
+            const firstY = document.createElement('h2');
+            const firstF = document.createElement('h2');
             firstB.innerHTML = bmwCar.car.brand;
             firstM.innerHTML = bmwCar.car.model;
-            firstV.innerHTML = bmwCar.car.generation;
-            if (bmwCar.car.generation === null) {
-                firstV.innerHTML = "no generation added"
-            }
-            const firstY = document.createElement('h2');
-            const firstCM = document.createElement('h2');
-            const firstF = document.createElement('h2');
             firstY.innerHTML = bmwCar.car.productionYear;
-            firstCM.innerHTML = bmwCar.car.carMileage;
-            firstF.innerHTML = bmwCar.car.fuelType;
+            firstF.innerHTML = bmwCar.car.fuelType.toLowerCase();
 
             const bmwPrices = document.createElement('h2');
             bmwPrices.innerHTML = `${bmwCar.price} $`;
@@ -49,13 +43,12 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=bmw`)
             //Adding classes
             bmwAnnouncementsDiv.className = ('announcementsMainDiv');
             bmwPhotos.className = ('announcementsPhotoClass');
-            divBMV.className = ('saleDescriptions')
-            divYMF.className = ('saleDescriptions')
+            divBMYF.className = ('saleDescriptions');
+            bmwPrices.className = ('prices');
 
             //Appending
-            divYMF.append(firstY, firstCM, firstF)
-            divBMV.append(firstB, firstM, firstV)
-            bmwDescriptions.append(divBMV, divYMF, bmwPrices);
+            divBMYF.append(firstB, firstM, firstY, firstF);
+            bmwDescriptions.append(divBMYF, bmwPrices);
             bmwPhotosDiv.append(bmwPhotos);
             bmwAnnouncementsDiv.append(bmwPhotosDiv);
             bmwAnnouncementsDiv.append(bmwDescriptions);
@@ -73,7 +66,7 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=audi`)
     .then(function(response) {
         const importedAudiCars = response.data
 
-        importedAudiCars.forEach((audiCar) => {
+        importedAudiCars.slice(-2).forEach((audiCar) => {
 
             //Creates two bmw cars div - main div
             const audiAnnouncementsDiv = document.createElement('div');
@@ -90,25 +83,17 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=audi`)
             const audiDescriptions = document.createElement('div');
 
             //Two description smaller divs
-            const divBMV = document.createElement('div');
-            const divYMF = document.createElement('div');
+            const divBMYF = document.createElement('div');
 
             //Smaller description divs children
             const firstB = document.createElement('h2');
             const firstM = document.createElement('h2');
-            const firstV = document.createElement('h2');
+            const firstY = document.createElement('h2');
+            const firstF = document.createElement('h2');
             firstB.innerHTML = audiCar.car.brand;
             firstM.innerHTML = audiCar.car.model;
-            firstV.innerHTML = audiCar.car.generation;
-            if (audiCar.car.generation === null) {
-                firstV.innerHTML = "no generation added"
-            }
-            const firstY = document.createElement('h2');
-            const firstCM = document.createElement('h2');
-            const firstF = document.createElement('h2');
             firstY.innerHTML = audiCar.car.productionYear;
-            firstCM.innerHTML = audiCar.car.carMileage;
-            firstF.innerHTML = audiCar.car.fuelType;
+            firstF.innerHTML = audiCar.car.fuelType.toLowerCase();;
 
             const audiPrices = document.createElement('h2');
             audiPrices.innerHTML = `${audiCar.price} $`;
@@ -116,13 +101,12 @@ axios.get(`http://localhost:8080/api/sale-announcements?brand=audi`)
             //Adding classes
             audiAnnouncementsDiv.className = ('announcementsMainDiv');
             audiPhotos.className = ('announcementsPhotoClass');
-            divBMV.className = ('saleDescriptions')
-            divYMF.className = ('saleDescriptions')
+            divBMYF.className = ('saleDescriptions');
+            audiPrices.className = ('prices');
 
             //Appending
-            divYMF.append(firstY, firstCM, firstF)
-            divBMV.append(firstB, firstM, firstV)
-            audiDescriptions.append(divBMV, divYMF, audiPrices);
+            divBMYF.append(firstB, firstM, firstY, firstF);
+            audiDescriptions.append(divBMYF, audiPrices);
             audiPhotosDiv.append(audiPhotos);
             audiAnnouncementsDiv.append(audiPhotosDiv);
             audiAnnouncementsDiv.append(audiDescriptions);
