44dab02081c810721b9c59395d93d37e85b6abba
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-10-02 13:22:12 +0200

    responsive progress

--- styles/responsiveMenuUser.js
+++ styles/responsiveMenuUser.js
@@ -1,6 +1,16 @@
-const toggleBtn = document.querySelector('.toggleNavTwo');
+const userToggleBtn = document.querySelector('.toggleNavTwo');
+const usersMenu = document.querySelector('#userNameSection');
+
+const nickName = document.querySelector('#nickName');
+const logout = document.querySelector('#logoutBtn');
+const sellCar = document.querySelector('#carSell');
+const yourSales = document.querySelector('#userSales');
 
 
-toggleBtn.addEventListener('click', () => {
-
+userToggleBtn.addEventListener('click', () => {
+    usersMenu.classList.toggle('active');
+    nickName.classList.toggle('active');
+    logout.classList.toggle('active');
+    sellCar.classList.toggle('active');
+    yourSales.classList.toggle('active');
 })
